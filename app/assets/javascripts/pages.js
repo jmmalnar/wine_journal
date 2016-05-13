d57c00c7d6966bb8b6318d839b0d6d82cf6679b1
Jason Malnar <jmmalnar@utexas.edu>
2016-05-13 13:03:17 -0500

    Finished the new home page

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,14 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready(function() {
+    $menuLeft = $('.pushmenu-left');
+    $title = $('#title');
+
+    $title.click(function() {
+        $(this).toggleClass('active');
+        $('.pushmenu-push').toggleClass('pushmenu-push-toright');
+        $menuLeft.toggleClass('pushmenu-open');
+    });
+});
