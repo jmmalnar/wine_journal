f5840b420cbebc8ea7553b068ebd91722e362650
Jason Malnar <jmmalnar@utexas.edu>
2016-06-30 17:24:05 -0500

    fixed search issue where it was only searching for second where

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,8 +1,7 @@
 class Wine < ActiveRecord::Base
 
   def self.search(search)
-    where("name LIKE ?", "%#{search}%")
-    where("grapes LIKE ?", "%#{search}%")
+    where("name LIKE ? OR grapes LIKE ?", "%#{search}%", "%#{search}%")
   end
 
 end
