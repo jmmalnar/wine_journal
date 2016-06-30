6928781d6053c36a6da226e7fbcc66b823dcc6cd
Jason Malnar <jmmalnar@utexas.edu>
2016-06-30 17:45:52 -0500

    Added ability to search for vintage and color as well

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,7 +1,7 @@
 class Wine < ActiveRecord::Base
 
   def self.search(search)
-    where("name LIKE ? OR grapes LIKE ?", "%#{search}%", "%#{search}%")
+    where("name LIKE ? OR grapes LIKE ? OR color LIKE ? OR vintage LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
   end
 
 end
