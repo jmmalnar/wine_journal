c912c748357ae56e60cc1b599f78faad604fb9e2
Jason Malnar <jmmalnar@utexas.edu>
2016-07-06 15:41:02 -0500

    Working on login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+
   root 'pages#home'
+
+  get 'signup'  => 'users#new'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :wines
   resources :users
 
-  get 'signup'  => 'users#new'
 end
