ef2f72606c26306e4191e84d2a94fdb9217e0fa3
Jason Malnar <jmmalnar@utexas.edu>
2016-07-06 13:10:23 -0500

    Finished users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  # GET /users/1/edit
+  def edit
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -14,6 +18,15 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
