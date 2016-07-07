3e21d5bbbc95f9607616a3f9fe4fe1f069e1685c
Jason Malnar <jmmalnar@utexas.edu>
2016-07-06 22:02:32 -0500

    Changed main page menu to check if logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the users my wine list page
+      log_in user
+      redirect_to wines_path
     else
-      flash[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
