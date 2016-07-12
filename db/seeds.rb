1880debcc794b3d104406ec46fff749fca88a70e
Jason Malnar <jmmalnar@utexas.edu>
2016-07-11 22:06:14 -0500

    Finished user edit, update, index and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(firstname: 'Example', lastname: 'User', email: 'example@user.com', password: 'password', password_confirmation: 'password')
+User.create!(
+    firstname: 'Example',
+    lastname: 'User',
+    email: 'example@user.com',
+    password: 'password',
+    password_confirmation: 'password',
+    admin: true)
 
 99.times do |n|
   firstname = Faker::Name.first_name
   lastname = Faker::Name.last_name
   email = "example-#{n+1}@user.com"
   password = 'password'
-  User.create!(firstname: firstname, lastname: lastname, email: email, password: password, password_confirmation: password)
+  User.create!(
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      password: password,
+      password_confirmation: password)
 end
